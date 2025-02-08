20bb03166b1d5f45dd37e9279926d0a380241cc4
rifqiraehan <rifqiraehan86@gmail.com>
2025-02-08 11:17:13 +0700

    add for import

---
+++ calc.py
@@ -0,0 +1,7 @@
+from rectangle_area import calculate_area
+
+# Example usage (for testing)
+length = 8
+width = 10
+area = calculate_area(length, width)
+print(f"The area of the rectangle is: {area}")
--- rectangle_area.py
+++ rectangle_area.py
@@ -1,9 +1,3 @@
 def calculate_area(length, width):
   """Calculates the area of a rectangle."""
   return length * width
-
-# Example usage (for testing)
-length = 8
-width = 10
-area = calculate_area(length, width)
-print(f"The area of the rectangle is: {area}")
