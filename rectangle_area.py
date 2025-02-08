80847541eabf6bd07162d3dadcc991d437787909
Muhammad Alfarrel Arya Mahardika <alfarrelmahardika@gmail.com>
2025-02-08 11:58:24 +0700

    ini update dari farrel

--- rectangle_area.py
+++ rectangle_area.py
@@ -1,3 +1,11 @@
 def calculate_area(length, width):
   """Calculates the area of a rectangle."""
   return length * width
+
+length = 10
+
+width = 20
+
+area = calculate_area(length,width)
+
+print(f"Jadi nilai persegi saya adalah {area}")
